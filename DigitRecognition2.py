21304493fd554a153e247ca7fdf4a2c857d935ff
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2025-10-11 20:44:56 -0400

    MNIST (90%)

--- DigitRecognition2.py
+++ DigitRecognition2.py
@@ -5,64 +5,36 @@ from keras.datasets import mnist, fashion_mnist
 (trainX, trainY), (testX, testY) = mnist.load_data()
 
 weights = np.zeros((28, 28, 10))
-new_weights = weights.copy()
+LEARNING_RATE = 0.01
 
-for j in range(3):
-    correct = 0
-    wrong = 0
-    for iteration in range(1000):
-        selection = trainX[iteration]
-        for x in range(28):
-            for y in range(28):
-                if selection[x][y] > 0:
-                    selection[x][y] = 1
+# training
+for i in range(60000):
+    selection = np.clip(0, 1, trainX[i % 60000])
+    actual = trainY[i % 60000]
+    expected = np.zeros(10)
+    expected[actual] = 1
 
-        #print(trainY[iteration])
-        output = [0] * 10
-        for x in range(28):
-            for y in range(28):
-                for z in range(10):
-                    output[z] += weights[x,y, z] * selection[x, y]
+    output = np.tensordot(selection, weights, axes=([0,1],[0,1]))
+    output = 1 / (1 + np.exp(-output))
+    #prediction = np.argmax(output)
 
-        #print(output)
-        prediction = np.argmax(output)
-        #print(prediction)
-        if prediction == trainY[iteration]:
-            correct += 1
-        else:
-            wrong += 1
+    errors = output - expected
+    weights -= np.outer(selection.flatten(), errors).reshape(28, 28, 10) * LEARNING_RATE
 
-        expected = [0] * 10
-        expected[trainY[iteration]] = 1
-        #print(expected)
-        output = np.array(output)
-        expected = np.array(expected)
-        errors = output - expected
-        #print(errors)
-        for i, error in enumerate(list(errors)):
-            for x in range(28):
-                for y in range(28):
-                    new_weights[x,y, i] -= error * selection[x, y] * 0.00001
-                    #if error != 0 and selection[x][y] > 0:
-                        #print(weights[x,y, i])
-    print(trainY[iteration])
-    print(output)
-    print(correct, wrong)
-    weights = new_weights.copy()
+    if i % 10000 == 0:
+        print(f"Training... ({i/60000*100:.2f}% Complete)")
 
+# testing
 correct = 0
 wrong = 0
-for i in range(100):
-    selection = testX[i]
-    output = [0] * 10
-    for x in range(28):
-        for y in range(28):
-            for z in range(10):
-                output[z] += weights[x, y, z] * selection[x, y]
+for i in range(10000):
+    selection = np.clip(0, 1, testX[i])
+    actual = trainY[i]
+    output = np.tensordot(selection, weights, axes=([0,1],[0,1]))
     prediction = np.argmax(output)
     if prediction == testY[i]:
         correct += 1
     else:
         wrong += 1
-    print(np.argmax(output), testY[i])
-print(correct, wrong)
+        #print(np.argmax(output), testY[i], "|", i)
+print(f"{correct / 10000*100:.2f}% Accuracy")
